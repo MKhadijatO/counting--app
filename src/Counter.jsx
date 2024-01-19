3db4932c5143b8329c58813cdbc39eaf0ee99a13
Khadijat <muhammadkhadijato@gmail.com>
2024-01-20 00:03:30 +0100

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Counter extends React.Component { 
+    render () {
+        return (
+            <div>
+                <p>Counter</p>
+            </div>
+        );
+    };
+);
+
+export default Counter;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div>
+            <img src="./src/images/react.png" alt="reactlogo" />
+            <p> React Course - CountOpedia</p>
+        </div>
+    )
+}
+
+export default Header;
