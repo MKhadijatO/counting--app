592863efa910ef8f7f59bf17f07e64585a46e3dd
Khadijat <muhammadkhadijato@gmail.com>
2024-01-23 23:48:42 +0100

    countopedia: alertmsg event method

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Counter extends React.Component { 
+handleAttack(){
+    alert('Attack clicked');
+}
+
+handleDefence(){
+    alert('Defence clicked');
+}
+
     render() { 
         return (
-            <div className='row'>
-                <h1>Counter</h1>
+            <div className='row text-center'>
+                <h1>Counter: </h1>
+                <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
+                <button onClick={this.handleDefence}style={{width:"200px"}}>-1</button>
             </div>
         );
     };
