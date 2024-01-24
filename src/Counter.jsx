46a779ac9594ed6af0b48f3c3c720f747cb95527
Khadijat <muhammadkhadijato@gmail.com>
2024-01-24 23:16:21 +0100

    update: using this.setState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React from "react";
 
-class Counter extends React.Component { 
-constructor(props) {
+class Counter extends React.Component {
+  constructor(props) {
     super(props);
     this.handleAttack = this.handleAttack.bind(this);
     this.handleDefence = this.handleDefence.bind(this);
-    this.state = { //state is immutable and unmodifiable
-        count: 0,
+    this.state = {
+      //state is immutable and unmodifiable
+      count: 0,
     };
-}
+  }
 
-
-    handleAttack(){
+  handleAttack() {
     // alert('Attack clicked');
-    this.setState({ count: this.state.count + 1 });
-}
+ 
 
-handleDefence(){
+    this.setState({ count: this.state.count + 100 });
+    this.setState({ count: this.state.count + 10 }) // only this get executed 
+  }
+
+  handleDefence() {
     // alert('Defence clicked');
     this.setState({ count: this.state.count - 1 });
-}
+  }
 
-    render() { 
-        return (
-            <div className='row '>
-                <h1>Counter: {this.state.count} </h1>
-                <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
-                <button onClick={this.handleDefence}style={{width:"200px"}}>-1</button>
-            </div>
-        );
-    };
-};
+  render() {
+    return (
+      <div className="row ">
+        <h1>Counter: {this.state.count} </h1>
+        <button onClick={this.handleAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handleDefence} style={{ width: "200px" }}>
+          -1
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,16 @@
-import logo from "../src/images/react.png"
+import logo from "../src/images/react.png";
 
 const Header = () => {
-    return(
-        <div className="pt-3 py-3 pl-2" style= {{borderBottom: "1px solid white"}}>
-            <img alt="logo" src={logo} style={{height:"35px", verticalAlign: "top"}}></img>
-            <span className="h2 pt-4 m-2 text-white" >React Course - CountOpedia</span>
-        </div>
-    )
-}
+  return (
+    <div className="pt-3 py-3 pl-2" style={{ borderBottom: "1px solid white" }}>
+      <img
+        alt="logo"
+        src={logo}
+        style={{ height: "35px", verticalAlign: "top" }}
+      ></img>
+      <span className="h2 pt-4 m-2 text-white">React Course - CountOpedia</span>
+    </div>
+  );
+};
 
 export default Header;
